a747da63aec48060485186ca43f8846395246b3f
Abinayasankar-co <abinayasankar@getmastery.ai>
2025-08-29 00:51:40 +0530

    Resolved Issues with TTS

--- Conversational-Agent/app.py
+++ Conversational-Agent/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import asyncio
 import os
-from utils.tts import speak
+from utils.tts import speak_stream
 from ui.components import show_url_input, show_summary, play_audio
 from utils.assistant_agent import RoyalEnfieldBikeAssistant
 from dotenv import load_dotenv
@@ -12,7 +12,6 @@ load_dotenv()
 st.set_page_config(page_title="Neo4j Voice Assistant", layout="wide")
 st.title("🔍 Neo4j Query Assistant with Voice")
 
-information = RoyalEnfieldBikeAssistant()
 
 if "query_history" not in st.session_state:
     st.session_state.query_history = []
@@ -20,26 +19,38 @@ if "query_history" not in st.session_state:
 user_input = st.text_input("Ask your question about the graph:")
 
 if user_input:
-    agent = RoyalEnfieldBikeAssistant(
-        llm_model="gpt-4",
-        tavily_api_key=os.environ["TAVILY_API_KEY"],
-        redis_url="redis://localhost:6379",
-        redis_cache_host="localhost",
-        redis_cache_port=6379,
-        redis_cache_db=0,
-        vector_index="bike_index"
-    )
-    if isinstance(user_input , str):
-      results , metadata = asyncio.run(agent.processed_query(user_query=user_input))
-      
-    st.session_state.query_history.append((user_input, results))
-    if results:
-        spoken_text = f"{results}"
-        speak(spoken_text)
-        st.success("Result spoken aloud.")
-    else:
-        speak("No results found.")
-        st.warning("No results found.")
+    try:
+        agent = RoyalEnfieldBikeAssistant(
+            llm_model="gpt-4",
+            tavily_api_key=os.environ["TAVILY_API_KEY"],
+            redis_url=  os.environ["REDIS_URL"], #Replace with Redis Url
+            redis_cache_host="localhost",
+            redis_cache_port=6379,
+            redis_cache_db=0,
+            vector_index="bike_index"
+        )
+
+    except Exception as e:
+        raise ValueError(f"Some Unexpected Value Error Arised : {e}")
+    
+    if isinstance(user_input, str):
+        results = asyncio.run(agent.processed_query(user_query=user_input))
+        content, metadata = None, None
+        if isinstance(results, dict) and "content" in results and "metadata" in results:
+            content, metadata = results["content"], results["metadata"]
+            print(content, metadata)
+        else:
+            content = results
+
+        st.session_state.query_history.append((user_input, content))
+        if content.strip():
+            audio_buf = speak_stream(user_input)
+            st.audio(audio_buf,format="audio/wav")
+            st.success("Result spoken aloud.")
+        else:
+            audio_buf = speak_stream("No results Found")
+            st.audio(audio_buf,format="audio/wav")
+            st.warning("No results found.")
 
 with st.sidebar:
     st.header("🕘 Query History")
--- Conversational-Agent/utils/tts.py
+++ Conversational-Agent/utils/tts.py
@@ -1,6 +1,20 @@
 import pyttsx3
+import tempfile
+import io
 
-def speak(text):
+def speak_stream(text):
     engine = pyttsx3.init()
-    engine.say(text)
+    buf = io.BytesIO()
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as fp:
+        filename = fp.name
+    engine.save_to_file(text, filename)
     engine.runAndWait()
+    with open(filename, "rb") as f:
+        buf.write(f.read())
+    buf.seek(0)
+    return buf
+
+
+if __name__ == "__main__":
+    txt = "Hello world"
+    speak(txt)
