4a366b3280f73735e126604665aec9aa16a3157a
Abinayasankar-co <abinayasankar@getmastery.ai>
2025-09-03 14:08:56 +0530

    Changes due to issues

--- Conversational-Agent/utils/tts.py
+++ Conversational-Agent/utils/tts.py
@@ -1,20 +1,9 @@
-import pyttsx3
+#import pyttsx3
 import tempfile
 import io
 from gtts import gTTS
 
-#Buffer and Streaming for faster transmission to user
-def speak_stream(text):
-    engine = pyttsx3.init()
-    buf = io.BytesIO()
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as fp:
-        filename = fp.name
-    engine.save_to_file(text, filename)
-    engine.runAndWait()
-    with open(filename, "rb") as f:
-        buf.write(f.read())
-    buf.seek(0)
-    return buf
+
 
 def speaker_stream(text, lang="en"):
     buf = io.BytesIO()
